ce6e45b8dd8c4fe66c9ffb57493f8dd6e1435750
victorgpt0 <175977320+victorgpt0@users.noreply.github.com>
2025-05-07 23:10:44 +0300

    remove bug in ALLOWED_HOSTS

--- kopa/settings.py
+++ kopa/settings.py
@@ -16,8 +16,7 @@ SECRET_KEY = 'django-insecure-0v$%y-54p*=!td!**sguksonez0oaf=t1)vxab==g8qv+mpaq$
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "mbadi.pythonanywhere.com",
-    "*"
+    "mbadi.pythonanywhere.com"
     ]
 
 
